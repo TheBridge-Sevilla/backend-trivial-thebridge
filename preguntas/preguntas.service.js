dd43857ceece3f501776c41f9ee8a79a836fba15
Ulyssbat <stewartbatallasminda@gmail.com>
2022-07-09 09:46:35 +0200

    añadimos funcion del a preguntas mediante postman

--- preguntas/preguntas.service.js
+++ preguntas/preguntas.service.js
@@ -16,27 +16,6 @@ async function create (body) {
   return await pregunta.save();
 }
 
-/* exports.delete = (req, res) => {
-  const id = req.params.id;
-  Pregunta.findByIdAndRemove(id)
-    .then(data => {
-      if (!data) {
-        res.status(404).send({
-          message: `No es posible eliminar la pregunta con id=${id}: la pregunta no ha sido encontrada!`
-        });
-      } else {
-        res.send({
-          message: "Pregunta eliminada correctamente"
-        });
-      }
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: "No se ha podido eliminar la pregunta con id=" + id
-      });
-    });
-}; */
-
 async function deleteOne (body) {
   console.log(body)
   return await Pregunta.deleteOne({ _id: body.id })
