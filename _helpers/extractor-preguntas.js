161527a2d5d8816189da37b0c31ce83d9a207809
idavid80 <dominguezbueno.david@gmail.com>
2022-09-27 12:51:00 +0200

    preguntas masivas pendiente terminar id categoria

--- _helpers/extractor-preguntas.js
+++ _helpers/extractor-preguntas.js
@@ -1,35 +1,49 @@
 const db = require("../_helpers/db");
-const Pregunta = db.Pregunta;
+const translate = require('translate-google')
 const Categoria = db.Categorias;
 
+// const Pregunta = db.Pregunta;
+
+Categoria.find().then(categoriaMongo => {let categoriaID = {}
+      for (categoriaLista of categoriaMongo) {
+        categoriaID[categoriaLista.nombre.en]= categoriaLista._id
+  } console.log(categoriaID )
+  }       
+)
+
 const fetch = (url) =>
-import("node-fetch").then(({ default: fetch }) => fetch(url));
-
-let preguntas = [];
-let preguntas_transformadas = [];
-
+  import("node-fetch").then(({ default: fetch }) => fetch(url));
 fetch(
-"https://opentdb.com/api.php?amount=10&category=22&difficulty=medium&type=multiple"
+  "https://opentdb.com/api.php?amount=50"
 )
-.then((response) => response.json())
-.then((data) =>
-  data.results.map((pregunta) => {
-    preguntas.push({
-      categoria: pregunta.category,
-      pregunta: pregunta.question,
-      correcta: pregunta.correct_answer,
-      incorrecta: pregunta.incorrect_answers,
-    });
-  })
-)
-.then(() => {
-  for (let pregunta of preguntas) {
-    console.log(pregunta)
-    let pregunta_transformada = {pregunta:{en:'hello',es:'hola'},opciones:{en:['one','two','three'],es:['uno','dos','tres']},categoria:undefined,solucion:'0'}
-    const preguntaNueva = new Pregunta(pregunta_transformada)
-    console.log(preguntaNueva)
-    preguntaNueva.save()
-    console.log('guardado')
+  .then((response) => response.json())
 
-}
-});
+  .then((data) =>
+    data.results.map((res) => {
+
+      let obtenerSolucion = Math.floor(Math.random() * 4)
+      let obtenerOpciones = res.incorrect_answers.splice(obtenerSolucion, (res.incorrect_answers.length + 1), res.correct_answer)
+      let todasOpciones = res.incorrect_answers.concat(obtenerOpciones)
+//let idCategoria = ObtenerCategoriaID(res.category)
+
+
+      translate({
+        question: res.question,
+        options: todasOpciones
+      }, { to: 'es' }).then(traduccion => {
+        let PreguntaTransformada = {
+          pregunta: { es: traduccion.question, en: res.question },
+          opciones: { es: traduccion.options, en: todasOpciones },
+          categoria: res.category,
+          solucion: obtenerSolucion
+        }
+
+        //console.log(PreguntaTransformada)
+
+        /*         let PreguntaInsertar = new Pregunta(PreguntaTransformada)
+                PreguntaInsertar.save() */
+      }).catch(err => {
+        console.error(err)
+      })
+    })
+  )
--- _helpers/traductor.js
+++
@@ -1,7 +0,0 @@
-const translate = require('translate-google')
-
-translate('Hello car', {to: 'es'}).then(res => {
-    console.log(res)
-}).catch(err => {
-    console.error(err)
-})
