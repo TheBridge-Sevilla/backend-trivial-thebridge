bff664cf20952c090519432cf8fe2bbacf09f8b5
idavid80 <dominguezbueno.david@gmail.com>
2022-09-30 20:11:57 +0200

    extractor de pregunta masiva terminado

--- _helpers/extractor-preguntas.js
+++ _helpers/extractor-preguntas.js
@@ -1,51 +1,64 @@
 const db = require("../_helpers/db");
-const translate = require('translate-google')
+const translate = require("translate-google");
 const Categoria = db.Categorias;
+const Pregunta = db.Pregunta;
 
-// const Pregunta = db.Pregunta;
-let categoriaID = {}
-Categoria.find().then(categoriaMongo => {
-  
-  for (categoriaLista of categoriaMongo) {
-    categoriaID[categoriaLista.nombre.en] = categoriaLista._id
-  }
+let categoriaID = {};
+Categoria.find()
+  .then((categoriaMongo) => {
+    for (categoriaLista of categoriaMongo) {
+      categoriaID[categoriaLista.nombre.en] = categoriaLista._id;
+    }
+  })
+  .then(() => {
+    const fetch = (url) =>
+      import("node-fetch").then(({ default: fetch }) => fetch(url));
+    fetch("https://opentdb.com/api.php?amount=50&type=multiple")
+      .then((response) => response.json())
+
+      .then((data) =>
+        data.results.map((res) => {
+          let obtenerSolucion = Math.floor(Math.random() * 4);
+          let obtenerOpciones = res.incorrect_answers.splice(
+            obtenerSolucion,
+            res.incorrect_answers.length + 1,
+            res.correct_answer
+          );
+          let todasOpciones = res.incorrect_answers.concat(obtenerOpciones);
+          let idCategoria = categoriaID[res.category];
+
+          translate(
+            {
+              question: res.question,
+              options: todasOpciones,
+            },
+            { to: "es" }
+          )
+            .then((traduccion) => {
+              let PreguntaTransformada = {
+                pregunta: { es: traduccion.question, en: res.question },
+                opciones: { es: traduccion.options, en: todasOpciones },
+                categoria: idCategoria,
+                solucion: obtenerSolucion,
+              };
+              comprobarPregunta(PreguntaTransformada.pregunta).then(
+                (duplicada) => {
+                  if (duplicada == false) {
+                    let PreguntaInsertar = new Pregunta(PreguntaTransformada);
+                    PreguntaInsertar.save();
+                  }
+                }
+              );
+            })
+            .catch((err) => {
+              console.error(err);
+            });
+        })
+      );
+  });
+
+async function comprobarPregunta(tituloPregunta) {
+  let preguntaRepetida = await Pregunta.find({ pregunta: tituloPregunta });
+  // Si es true la pregunta está repetida
+  return preguntaRepetida.length > 0 ? true : false;
 }
-).then(()=> {
-  const fetch = (url) =>
-  import("node-fetch").then(({ default: fetch }) => fetch(url));
-fetch(
-  "https://opentdb.com/api.php?amount=50&type=multiple"
-)
-  .then((response) => response.json())
-
-  .then((data) =>
-    data.results.map((res) => {
-
-      let obtenerSolucion = Math.floor(Math.random() * 4)
-      let obtenerOpciones = res.incorrect_answers.splice(obtenerSolucion, (res.incorrect_answers.length + 1), res.correct_answer)
-      let todasOpciones = res.incorrect_answers.concat(obtenerOpciones)
-      let idCategoria = categoriaID[res.category]
-
-
-      translate({
-        question: res.question,
-        options: todasOpciones
-      }, { to: 'es' }).then(traduccion => {
-        let PreguntaTransformada = {
-          pregunta: { es: traduccion.question, en: res.question },
-          opciones: { es: traduccion.options, en: todasOpciones },
-          categoria: idCategoria,
-          solucion: obtenerSolucion
-        }
-
-        console.log(PreguntaTransformada)
-
-        /*         let PreguntaInsertar = new Pregunta(PreguntaTransformada)
-                PreguntaInsertar.save() */
-      }).catch(err => {
-        console.error(err)
-      })
-    })
-  )
-})
-
