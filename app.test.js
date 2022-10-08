11246971428880509bc5dd4d8d75a35754e27085
xaxi <francisco.perez@thebridgeschool.es>
2022-10-08 09:44:11 +0200

    pruebas

--- app.test.js
+++ app.test.js
@@ -2,20 +2,37 @@ const app = require("./server")
 const supertest = require("supertest")
 const request = supertest(app)
 
+// Preguntas
 describe("/preguntas", () => {
-    it("should return a response", async () => {
+    it("Debería responder con estado 200", async () => {
         const response = await request.get("/preguntas")
         expect(response.status).toBe(200)
     })
 })
 
-describe("/categorias", () => {
-    it("should return a response", async () => {
-        const response = await request.get("/categorias")
-        expect(response.status).toBe(200)
+describe("/preguntas", () => {
+    it("Todas las preguntas deberían de tener la estructura de nuestro modelo de Preguntas.", async () => {
+        const response = await request.get("/preguntas")
+        listaPreguntas = JSON.parse(response.text)
+        for (pregunta of listaPreguntas){
+            expect(pregunta).toHaveProperty('pregunta')
+            expect(pregunta).toHaveProperty('opciones')
+            expect(pregunta).toHaveProperty('categoria')
+            expect(pregunta).toHaveProperty('solucion')
+        }
     })
 })
 
+// Categorias
+describe("/categorias", () => {
+    it("Debería contener 24 elementos", async () => {
+        const response = await request.get("/categorias")
+        json = JSON.parse(response.text)
+        expect(json).toHaveLength(24)
+    })
+})
+
+// Partidas
 describe("/partidas", () => {
     it("should return a response", async () => {
         const response = await request.get("/partidas")
