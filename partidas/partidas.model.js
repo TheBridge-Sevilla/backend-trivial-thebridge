99faf6cf87d6b90c0b3e1b67fecd6f42e2a234a7
idavid80 <dominguezbueno.david@gmail.com>
2022-10-20 10:12:35 +0200

    modelo partida actualizado

--- partidas/partidas.model.js
+++ partidas/partidas.model.js
@@ -2,12 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
-  jugadorID: String,  //id del jugador
-  nombre: String,   //nombre del jugador
-  categoria: { type: Schema.ObjectId, ref: 'Categoria' },
+  nombre: String, //nombre del jugador
+  categoria: { type: Schema.ObjectId, ref: "Categoria" },
   puntuacion: Number,
-  
-
+  fecha: Date,
 });
 
 module.exports = mongoose.model("Partida", schema);
