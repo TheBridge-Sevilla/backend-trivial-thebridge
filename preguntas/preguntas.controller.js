f6e18779b9d4ba862ef873ba8df7fa9c7e5a06fd
P-Kempf <paulonwein@gmail.com>
2022-11-04 19:58:16 +0100

    agregado campos adicionales al ejemplo de preguntas/categorias

--- preguntas/preguntas.controller.js
+++ preguntas/preguntas.controller.js
@@ -13,6 +13,60 @@ router.get('/', getAll)
 router.post('/', create)
 router.post('/categoria', getPreguntasByCategory)
 
+/**
+ * @swagger
+ * /preguntas/categoria:
+ *   post:
+ *     tags:
+ *     - preguntas
+ *     summary: Devuelve una lista de preguntas filtrada por la categoria seleccionada.
+ *     operationId: categoria._id
+ *     requestBody:
+ *       description: Envia el ID de la pregunta seleccionada
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *                id:
+ *                 type: string
+ *                 description: El ID de la categoria.
+ *                 example: 634ef53c64362d730580cd9a
+ *     responses:
+ *       200:
+ *         description: Successful operation
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   pregunta:
+ *                     type: string
+ *                     description: La pregunta del trivial de la categoria elegida.
+ *                     example: 
+ *                      es: El lugar de nacimiento y muerte de Alberto Durero fue en...
+ *                      en: Albrecht Dürer's birthplace and place of death were in...
+ *                   opciones:
+ *                     description: Las posibles opciones a elegir de la pregunta.
+ *                     example: 
+ *                      es: [ Augsburgo, Bamberg,Berlín,Nuremberg ]
+ *                      en: [ Augsburgo, Bamberg,Berlín,Nuremberg ]  
+ *                   _id:
+ *                     type: string
+ *                     description: El ID de la categoria.
+ *                     example: 635102dd9ef0f77c5801cfb4 
+ *                   categoria:
+ *                     type: string
+ *                     description: El ID de la categoria.
+ *                     example: 635102dd9ef0f77c5801cfb4 
+ *                   solucion:
+ *                     type: number
+ *                     description: Posicion de la respuesta correcta .
+ *                     example: 3                
+ */
+
 module.exports = router
 
 function getAll (req, res, next) {
