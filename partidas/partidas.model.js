22ee64072c4cf6abfbe98e6e65e53e87b2aea323
idavid80 <dominguezbueno.david@gmail.com>
2022-11-08 19:45:34 +0100

    añadir id modelo partida y eliminar jugador

--- partidas/partidas.model.js
+++ partidas/partidas.model.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
+  idUsuario: String, //id del usuario Firebase
   nombre: String, //nombre del jugador
   categoria: { type: Schema.ObjectId, ref: "Categoria" },
   puntuacion: Number,
